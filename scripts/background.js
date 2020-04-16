2d24184e7c9d82c455cce569aefb29d4d2707642
PB <brak>
2020-04-16 04:07:14 +0200

    minor changes

--- scripts/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(function (message) {
-  if (message == "runContentScript") {
-    setTimeout(() => {
-      chrome.tabs.executeScript({
-        file: "logic.js",
-      });
-    }, 5000);
-  }
-});
